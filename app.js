9f94b5fdad30322c645fc0dabeb4df13624e10d8
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-05 01:55:14 +0300

    2 way databinding

--- app.js
+++ app.js
@@ -1,18 +1,10 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: 'Volkan',
-		job: 'Hacker',
-		age: 22,
-		x:0,
-		y:0
+		name: '',
+		age:''
 	},
 	methods: {
-		logName: function () {
-			console.log("log name");
-		},
-		logAge: function () {
-			console.log("log age");
-		},
+
 	}
 });
