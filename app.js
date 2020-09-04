62e0ecb674b575c52a8101c96404c87352e7fc59
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-05 02:03:16 +0300

    computed properties

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 new Vue({
 	el: '#vue-app',
 	data: {
-		name: '',
-		age:''
+		a: 1,
+		b:0,
+		age: 20
 	},
 	methods: {
-
+	},
+	computed:{
+		atoAge: function () {
+			return this.age+this.a
+		},
+		btoAge: function () {
+			return this.age + this.b
+		}
 	}
 });
