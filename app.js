483a100a2bd32889e64b316ea60023d26a2066d8
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-05 01:38:36 +0300

    events

--- app.js
+++ app.js
@@ -2,6 +2,21 @@ new Vue({
 	el: '#vue-app',
 	data: {
 		name: 'Volkan',
-		job: 'Hacker'
+		job: 'Hacker',
+		age: 22,
+		x:0,
+		y:0
+	},
+	methods: {
+		add: function (inc) {
+			this.age += inc;
+		},
+		subtract: function (dec) {
+			this.age -= dec;
+		},
+		updateXY: function (event) {
+			this.x = event.offsetX;
+			this.y = event.offsetY;
+		}
 	}
 });
