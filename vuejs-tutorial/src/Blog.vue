497c3b1d16f6d7e23e81f991e5cadc5b5071d671
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-06 02:11:33 +0300

    nesting components

---
+++ vuejs-tutorial/src/Blog.vue
@@ -0,0 +1,18 @@
+<template>
+  <ul>
+      <li v-for="blog in blogs">{{blog}}</li>
+  </ul>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      blogs: ['hello', 'world']
+    }
+  }
+}
+</script>
+
+<style>
+</style>
--- vuejs-tutorial/src/main.js
+++ vuejs-tutorial/src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Blogs from './Blog.vue'
+
+Vue.component('blogs', Blogs)
 
 new Vue({
   el: '#app',
