1df2188105ec51c69a356852de9efc1839924dd7
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-08 17:30:04 +0300

    get requests

--- vuejs-tutorial/src/App.vue
+++ vuejs-tutorial/src/App.vue
@@ -1,14 +1,16 @@
 <template>
   <div>
-    <add-blog></add-blog>
+    <show-blog></show-blog>
   </div>
 </template>
 
 <script>
 import addBlog from "./components/addBlog.vue";
+import showBlog from "./components/showBlogs.vue";
 export default {
   components: {
     "add-blog": addBlog,
+    "show-blog": showBlog,
   },
   data() {
     return {};
---
+++ vuejs-tutorial/src/components/showBlogs.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="show-blogs">
+    <h1>All Blog Articals</h1>
+    <div v-for="post in posts" class="single-blog">
+        <h3>{{post.title}}</h3>
+        <p>{{post.body}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  },
+  data() {
+    return {
+        posts:[]
+    };
+  },
+  methods:{
+    
+  },
+  created(){
+    this.$http.get('https://jsonplaceholder.typicode.com/posts/').then((data)=>{
+        this.posts = data.body.slice(0,10);
+    })
+  }
+};
+</script>
+
+<style>
+#show-blogs{
+    max-width: 600px;
+    margin: 0 auto;
+
+    /* padding: 10px; */
+}
+.single-blog{
+    padding: 20px;
+    margin: 20px 0;
+    box-sizing: border-box;
+    background: #eee;
+}
+</style>
