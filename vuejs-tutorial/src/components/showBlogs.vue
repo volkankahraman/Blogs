e58b256d794d2c5706d3100525116660fcab9a13
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-08 17:57:45 +0300

    custom search

--- vuejs-tutorial/src/components/showBlogs.vue
+++ vuejs-tutorial/src/components/showBlogs.vue
@@ -1,7 +1,8 @@
 <template>
   <div v-theme="'wide'" id="show-blogs">
     <h1>All Blog Articals</h1>
-    <div v-for="post in posts" class="single-blog">
+    <input type="text" v-model="search" placeholder="search">
+    <div v-for="post in filteredBlogs" class="single-blog">
         <h3 v-rainbow>{{post.title | to-uppercase}}</h3>
         <p>{{post.body | snippet}}</p>
     </div>
@@ -14,11 +15,19 @@ export default {
   },
   data() {
     return {
-        posts:[]
+        posts:[],
+        search: ''
     };
   },
   methods:{
     
+  },
+  computed:{
+    filteredBlogs:function () {
+      return this.posts.filter((blog) => {
+        return blog.title.includes(this.search)
+      })
+    }
   },
   created(){
     this.$http.get('https://jsonplaceholder.typicode.com/posts/').then((data)=>{
