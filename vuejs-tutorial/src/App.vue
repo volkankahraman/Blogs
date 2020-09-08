fc42b6a09b5f804e03356f3c27bf661d4d1439c1
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-08 15:43:07 +0300

    Slots

--- vuejs-tutorial/src/App.vue
+++ vuejs-tutorial/src/App.vue
@@ -1,26 +1,39 @@
 <template>
 <div>
-  <app-header></app-header>
-  <blogs></blogs>
-  <app-footer></app-footer>
+  <form-helper>
+    <div slot="form-header">
+      <h3>Login</h3>
+    </div>
+    <div slot="form-fields">
+      <label for="id">Username :</label>
+      <input type="text" id="id"><br>
+      <label for="password">password :</label>
+      <input type="password" id="password">
+    </div>
+    <div slot="form-controls">
+      <button @click.prevent="handleSubmit"> Login </button>
+    </div>
+  </form-helper>
 </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import Footer from "./components/Footer.vue";
-import Blogs from "./components/Blogs.vue";
+import formHelper from './components/formHelper.vue'
+
 export default {
   components: {
-    'app-header':Header,
-    'app-footer':Footer,
-    'blogs': Blogs
+    'form-helper': formHelper,
   },
   data () {
     return {
 
     }
-  }
+  },
+  methods: {
+    handleSubmit: function () {
+     alert(); 
+    }
+  },
 }
 </script>
 
