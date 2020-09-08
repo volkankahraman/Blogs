baa733bd91c87918eb3e6f2bec25787db1228ab8
Volkan kahraman <khrmnvolkan@gmail.com>
2020-09-08 18:03:40 +0300

    local directives and filters

--- vuejs-tutorial/src/components/showBlogs.vue
+++ vuejs-tutorial/src/components/showBlogs.vue
@@ -1,53 +1,64 @@
 <template>
   <div v-theme="'wide'" id="show-blogs">
     <h1>All Blog Articals</h1>
-    <input type="text" v-model="search" placeholder="search">
+    <input type="text" v-model="search" placeholder="search" />
     <div v-for="post in filteredBlogs" class="single-blog">
-        <h3 v-rainbow>{{post.title | to-uppercase}}</h3>
-        <p>{{post.body | snippet}}</p>
+      <h3 v-rainbow>{{post.title | to-uppercase}}</h3>
+      <p>{{post.body | snippet}}</p>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  components: {
-  },
+  components: {},
   data() {
     return {
-        posts:[],
-        search: ''
+      posts: [],
+      search: "",
     };
   },
-  methods:{
-    
-  },
-  computed:{
-    filteredBlogs:function () {
+  methods: {},
+  computed: {
+    filteredBlogs: function () {
       return this.posts.filter((blog) => {
-        return blog.title.includes(this.search)
-      })
-    }
+        return blog.title.includes(this.search);
+      });
+    },
   },
-  created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts/').then((data)=>{
-        this.posts = data.body.slice(0,10);
-    })
-  }
+  created() {
+    this.$http
+      .get("https://jsonplaceholder.typicode.com/posts/")
+      .then((data) => {
+        this.posts = data.body.slice(0, 10);
+      });
+  },
+  filters: {
+    toUppercase: function (value) {
+      return value.toUpperCase();
+    },
+  },
+  directives: {
+    rainbow: {
+      bind(el, binding, vnode) {
+        el.style.color = "#" + Math.random().toString().slice(2, 8);
+      },
+    },
+  },
 };
 </script>
 
 <style>
-#show-blogs{
-    max-width: 600px;
-    margin: 0 auto;
+#show-blogs {
+  max-width: 600px;
+  margin: 0 auto;
 
-    /* padding: 10px; */
+  /* padding: 10px; */
 }
-.single-blog{
-    padding: 20px;
-    margin: 20px 0;
-    box-sizing: border-box;
-    background: #eee;
+.single-blog {
+  padding: 20px;
+  margin: 20px 0;
+  box-sizing: border-box;
+  background: #eee;
 }
 </style>
--- vuejs-tutorial/src/main.js
+++ vuejs-tutorial/src/main.js
@@ -5,20 +5,13 @@ import VueResource from "vue-resource";
 Vue.use(VueResource);
 
 //Filters
-Vue.filter('to-uppercase', function (value) {
-  return value.toUpperCase();
-})
 
 Vue.filter('snippet', function (value) {
   return value.slice(0,100).toString()+ '...'
 })
 
 //Custom Directives
-Vue.directive('rainbow',{
-  bind(el,binding,vnode){
-    el.style.color = "#" + Math.random().toString().slice(2,8);
-  }
-})
+
 Vue.directive('theme',{
   bind(el,binding,vnode){
     if(binding.value == "wide"){
